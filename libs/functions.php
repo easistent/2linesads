967deed4493df7beacfe87e681b6cd8d937b5a9b
Marko McLion <mclion@gmail.com>
2012-11-12 00:27:06 +0100

    fixed ip bug

--- libs/functions.php
+++ libs/functions.php
@@ -20,10 +20,11 @@ function check_ip($ip, $cookie, $add_id, $event_time, $event_timeout, $event_typ
 
 	$result = $db->query("SELECT event_time
 							FROM 2linesads_log
-							WHERE add_id=$add_id
-							AND cookie='$cookie'
+							WHERE add_id = $add_id
+							AND cookie = '$cookie'
 							AND event_type = '$event_type'
 							AND event_time < ($event_time - $event_timeout)
+							AND ip = '$ip'
 							") or die('err:'.$db -> error);
     if  ($result->num_rows > 0) {
         return true; 
@@ -32,18 +33,18 @@ function check_ip($ip, $cookie, $add_id, $event_time, $event_timeout, $event_typ
         $db->query("UPDATE LOW_PRIORITY 2linesads_log
 					   SET event_time=NOW(),
 					   ip = '$ip'
-					   WHERE cookie='$cookie'
-					   AND add_id='$add_id'
-					   AND event_type='$event_type'
+					   WHERE cookie = '$cookie'
+					   AND add_id = '$add_id'
+					   AND event_type = '$event_type'
 					   ") or die('err:'.$db -> error);
         if ($db->affected_rows <= 0) {
             $db->query("INSERT INTO 2linesads_log
 					   SET event_time=NOW(),
 					   ip = '$ip',
-					   cookie='$cookie',
-					   add_id='$add_id',
+					   cookie = '$cookie',
+					   add_id = '$add_id',
 					   `word` = '$word',
-					   event_type='$event_type'") or die('err:'.$db -> error);
+					   event_type = '$event_type'") or die('err:'.$db -> error);
         }
         return false; 
     
